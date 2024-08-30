63eb5cf3ccacbcd3704b14ad2af5c8ea914f9fae
kso <kso@mz.co.kr>
2024-08-30 14:19:03 +0900

    initialize project

---
+++ src/test/java/com/mzc/springapp/SpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mzc.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
