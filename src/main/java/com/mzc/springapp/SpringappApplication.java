aca280031f4376f6074665fd1a32da7540e47ff6
kso <kso@mz.co.kr>
2024-08-30 15:04:42 +0900

    Update SpringApplication

--- src/main/java/com/mzc/springapp/SpringappApplication.java
+++ src/main/java/com/mzc/springapp/SpringappApplication.java
@@ -3,10 +3,14 @@ package com.mzc.springapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringappApplication {
 
 	public static void main(String[] args) {
+		log.info("SpringappApplication");
 		SpringApplication.run(SpringappApplication.class, args);
 	}
 
